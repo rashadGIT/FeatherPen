2f908a0a80577150e3269f1012db55da88975fe2
RashadB88 <Rashad.barnett@gmail.com>
2023-02-10 19:54:51 -0600

    Adding new folders

--- backend/src/routes/chatGPT.ts
+++ backend/src/routes/chatGPT.ts
@@ -1,27 +1,23 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance as FastInstance } from "fastify";
 import { Configuration, OpenAIApi } from "openai";
 
-function chatGPT(fastify: FastifyInstance, options: any, done: any) {
-  fastify.get("/chatGPT", options, async function (request, reply) {
-    const body: any = request.body
-    const message  = body?.message
-    const configuration = new Configuration({
-        apiKey: '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',
+function chatGPT(fastify: FastInstance, options: any, done: any) {
+    fastify.post('/chatGPT', options, async (request) => {
+        const body: any = request.body
+        const message = body?.message
+        const configuration = new Configuration({apiKey: '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'})
+        const openAi = new OpenAIApi(configuration)
+        const response = await openAi.createCompletion({
+            model: 'text-davinci-003',
+            prompt: message || 'Tell Me about puppies in less than 100 words.',
+            temperature: 0.3,
+            max_tokens: 200,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        })
+        return { aiResponse: response?.data?.choices[0]?.text }
     })
-    const openai = new OpenAIApi(configuration)
-
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: message || 'Tell Me about cars in less than 100 words.',
-        temperature: 0.3,
-        max_tokens: 200,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-    })
-    
-    return { aiResponse:response.data.choices[0].text }
-  });
-  done();
+    done()
 }
 export default chatGPT;
---
+++ mobileUI/src/App.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from "react-native";
+
+import Tts from 'react-native-tts';
+import axios from "axios";
+
+function App() : JSX.Element {
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+  const [url, setUrl] = useState('http://192.168.1.10:3000/chatGPT');
+
+  const speak = (msg = 'No message given') => {
+    Tts.speak(msg)
+  };
+  
+  const getData = async () => {
+
+    const chatGPT = await axios
+        .post(url, {
+            message: 'Tell me about summer.',
+        })
+        .then((response) => {
+            return response.data
+        })
+    const chat = chatGPT?.aiResponse;
+    setName(name);
+    speak(chat);
+    setText(chat);
+  };
+
+  useEffect(() => {getData()},[]);
+
+  return (
+    <View style={boxStyles.container}>
+      <Text style={boxStyles.text}>{text}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUrl}
+        value={url}
+      />
+      <Button title="Generate New Story" onPress={() => getData()} />
+      <Text style={boxStyles.text}>{url}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  submitButton: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+  },
+});
+
+const boxStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 23,
+    lineHeight: 25,
+    fontWeight: "bold",
+    paddingHorizontal: 20,
+    bottom: 0,
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 42,
+    textAlignVertical: "bottom",
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+    bottom: 0,
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+  },
+});
+
+
+
+export default App;
